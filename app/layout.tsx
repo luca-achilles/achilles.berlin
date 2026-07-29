8e5136a2f269e28881b2dbb1ae5ebdf4b97ea273
Luca Achilles <luca@achilles.berlin>
2026-07-29 23:12:47 +0200

    feature: add basic plausible analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import { ReactNode } from 'react';
+import PlausibleProvider from 'next-plausible';
 
 const poppins = Poppins({
   variable: '--font-poppins',
@@ -33,7 +34,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <PlausibleProvider>
+        <body className={`${poppins.variable} antialiased`}>{children}</body>
+      </PlausibleProvider>
     </html>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from 'next';
+import { withPlausibleProxy } from 'next-plausible';
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPlausibleProxy({
+  src: 'https://plausible.achilles.berlin/js/pa-5S8FvFNJ6-I7q2DS1hP6n.js',
+})({
   output: 'standalone',
   images: {
     remotePatterns: [new URL('https://is1-ssl.mzstatic.com/image/thumb/**')],
   },
-};
+});
 
 export default nextConfig;
